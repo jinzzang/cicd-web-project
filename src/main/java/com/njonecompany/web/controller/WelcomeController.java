5c80468516e03f777d49b6ebf91f0a132d58ac0d
Cho Jin Ho <whwlsgh1111@naver.com>
2023-10-10 13:21:06 +0900

    error test

--- src/main/java/com/njonecompany/web/controller/WelcomeController.java
+++ src/main/java/com/njonecompany/web/controller/WelcomeController.java
@@ -14,7 +14,7 @@ public class WelcomeController {
     private final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
 
     @GetMapping("/")
-    public String index(Modeㅇl model) {
+    public String index(Model model) {
         logger.debug("Welcome to njonecompany.com...");
         // System.out.println("소나큐브");
         model.addAttribute("msg", getMessage());
